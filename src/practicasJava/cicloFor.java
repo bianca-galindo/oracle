529070a99b7db5097de47b6d8d4733b2ac0f1032
Bianca Galindo <bianca.yamileth.galindo@outlook.com>
2023-03-01 18:17:07 -0600

    Definición de ciclo for

--- src/practicasJava/cicloFor.java
+++ src/practicasJava/cicloFor.java
@@ -1,9 +1,14 @@
 package practicasJava;
 
+/*Definición:  Es un ciclo que permite la definición inicial del numero de repeticiones de una instruccion, al igual 
+ * que while, depende de una instrucción condicional valida.  */
 public class cicloFor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// (Crear variable e inicializar; validación ; incrementarvariable)
+		for(int contador=0; contador<=10; contador++) { 
+			System.out.println(contador);
+		}
 
 	}
 
